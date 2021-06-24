4ea69a2a43fe77fe8fa9a3b23e8d11ecf63c257a
gauseen <gauseen@gmail.com>
2021-06-24 14:52:36 +0800

    chore: update cli

--- src/index.js
+++ src/index.js
@@ -1,47 +1,115 @@
 const path = require('path');
-const fs = require('fs');
+const shell = require('shelljs');
 const commander = require('commander');
+const merge = require('lodash/merge');
+const { Table } = require('console-table-printer');
 const {
+  printer,
+  getPrintTableData,
   getAllConfigInfo,
   getGumrcInfo,
   setGumrcInfo,
   getGlobalGitUserConfig,
   getUsingGitUserConfig,
 } = require('./utils');
-const merge = require('lodash/merge');
+const { option } = require('commander');
 
 const program = new commander.Command('gum');
 
 program.command('list').description('List all the user config group').action(onList);
 
 program
-  .command('add <group_name>')
-  .description('Add one group for user config')
-  .option('--name <user_name>', 'user name')
-  .option('--email <user_email>', 'user email')
-  .action(onAdd);
+  .command('set <group-name>')
+  .description('Set one group for user config')
+  .option('--name <user-name>', 'user name')
+  .option('--email <user-email>', 'user email')
+  .action(onSet);
+
+program
+  .command('use <group-name>')
+  .description('Set one group for user config')
+  .option('--global', 'git config --global')
+  .action(onUse);
+
+program.command('delete <group-name>').description('Delete one group').action(onDelete);
 
 program.parse(process.argv);
 
 function onList() {
   const allConfig = getAllConfigInfo();
-  console.table(allConfig);
-  // console.log('gitUserConfig: ', gitUserConfig);
-  // const listStr = Object.keys(currentGumrcInfo).map((groupName) => {
-  //   const user = currentGumrcInfo[groupName];
-  //   return `${groupName} name="${user.name}" email="${user.email}" `;
-  // });
+  const using = getUsingGitUserConfig();
+  const tableData = getPrintTableData(allConfig);
+
+  // currently used user info
+  printer(`Currently used name=${using.name} email=${using.email}`, 'yellow');
+
+  // git user config group list
+  const pt = new Table();
+  pt.addRows(tableData, { color: 'cyan' });
+  pt.printTable();
 }
 
-function onAdd(groupName, options) {
+function onSet(groupName, options) {
   const newGroup = {
     [groupName]: options,
   };
 
-  setGumrcInfo(newGroup, (err) => {
+  if (!options.name && !options.email) {
+    printer(`Name and Email option must have one`, 'red');
+    return process.exit(1);
+  }
+
+  const gumrcInfo = getGumrcInfo();
+  const finalGumrcInfo = merge(gumrcInfo, newGroup);
+
+  setGumrcInfo(finalGumrcInfo, (err) => {
     if (err) {
-      return;
+      return process.exit(1);
     }
-    console.log('set success');
+    printer(`set ${groupName} group success`, 'green');
+  });
+}
+
+function onUse(groupName, options) {
+  const allConfigInfo = getAllConfigInfo();
+  const user = allConfigInfo[groupName];
+
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+  }
+
+  if (user) {
+    const g = options.global ? `--global` : '';
+
+    if (shell.exec(`git config ${g} user.name ${user.name}`).code !== 0) {
+      shell.echo('Error: Git config user.name failed');
+    }
+    if (shell.exec(`git config ${g} user.email ${user.email}`).code !== 0) {
+      shell.echo('Error: Git config user.email failed');
+    }
+
+    const using = getUsingGitUserConfig();
+
+    if (options.global) {
+      const globalGitUser = getGlobalGitUserConfig();
+      printer(`Global using name=${globalGitUser.name} email=${globalGitUser.email}`, 'green');
+    }
+
+    printer(`Currently used name=${using.name} email=${using.email}`, 'yellow');
+  } else {
+    printer(`${groupName} group name invalid`, 'red');
+  }
+}
+
+function onDelete(groupName) {
+  const gumrcInfo = getGumrcInfo();
+  gumrcInfo[groupName] && delete gumrcInfo[groupName];
+
+  setGumrcInfo(gumrcInfo, (err) => {
+    if (err) {
+      return process.exit(1);
+    }
+    printer(`delete ${groupName} group success`, 'green');
   });
 }
--- src/utils.js
+++ src/utils.js
@@ -7,39 +7,70 @@ const parse = require('parse-git-config');
 
 const GUMRC = path.join(process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'], '.gumrc');
 
-function getAllConfigInfo() {
-  const globalInfo = getGlobalGitUserConfig();
-  const currentGumrcInfo = getGumrcInfo();
-  const allInfo = merge(globalInfo, currentGumrcInfo);
-  return allInfo;
-}
-
 function getGumrcInfo() {
   return fs.existsSync(GUMRC) ? ini.decode(fs.readFileSync(GUMRC, 'utf-8')) : {};
 }
 
-function setGumrcInfo(newGroup, callback) {
-  const gumrcInfo = getGumrcInfo();
-  const finalGumrcInfo = merge(gumrcInfo, newGroup);
-  console.log('finalGumrcInfo: ', JSON.stringify(finalGumrcInfo));
+function setGumrcInfo(finalGumrcInfo, callback) {
   fs.writeFile(GUMRC, ini.encode(finalGumrcInfo), callback);
 }
 
+function getAllConfigInfo() {
+  const globalInfo = getGlobalGitUserConfig();
+  const currentGumrcInfo = getGumrcInfo();
+  const allInfo = merge({ global: globalInfo }, currentGumrcInfo);
+  return allInfo;
+}
+
 function getUsingGitUserConfig() {
-  const currentProjectConfig = parse.sync();
-  const config = currentProjectConfig.user || getGlobalGitUserConfig().global;
-  return config;
+  const name = getProjectGitUserConfig()?.name || getGlobalGitUserConfig()?.name;
+  const email = getProjectGitUserConfig()?.email || getGlobalGitUserConfig()?.email;
+  return {
+    email,
+    name,
+  };
+}
+
+function getProjectGitUserConfig() {
+  return parse.sync().user;
 }
 
 function getGlobalGitUserConfig() {
   const global = gitConfigPath('global');
-  return {
-    global: parse.sync({ path: global }).user,
+  return parse.sync({ path: global }).user;
+}
+
+function getPrintTableData(data) {
+  const tableData = Object.keys(data).map((groupName, index) => {
+    const { name, email } = data[groupName] || {};
+    return {
+      ['group-name']: groupName,
+      name,
+      email,
+    };
+  });
+
+  return tableData;
+}
+
+// https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
+function printer(val, color) {
+  const colorMap = {
+    red: '\x1b[31m',
+    yellow: '\x1b[33m',
+    green: '\x1b[32m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
   };
+
+  console.log(' ');
+  console.log(`${colorMap[color]}%s\x1b[0m`, val);
 }
 
 module.exports = {
   GUMRC,
+  printer,
+  getPrintTableData,
   getAllConfigInfo,
   getGumrcInfo,
   setGumrcInfo,
